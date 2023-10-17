4fb911d9d698d0336a6ef99f04e1413959f06f65
sosam1 <sosam1763@gmail.com>
2023-10-17 10:55:36 -0300

    ruta eficiente

--- js/choferes.js
+++ js/choferes.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+
+
 let URL_PAQUETES_PARA_ENTREGAR = "http://127.0.0.1:8000/api/v1/paquetes/paraEntregar/"
 
 let URL_DETALLE_PAQUETE = "http://127.0.0.1:8000/api/v1/paquetes/paraEntregar/detalle/"
@@ -41,13 +46,55 @@ fetch(URL_PAQUETES_PARA_ENTREGAR)
                 <tr>
                     <td>${datos_paquete.id}</td>
                     <td>${datos_paquete.direccion}</td>
-                    <td><a href="./prueba.html">Google maps</a></td>
+                    <td><a class="googleMaps" id="paquete${datos_paquete.id}" href="#">Google maps</a></td>
                     <td>Jose Sanchez</td>
                     <td>#1</td>
                     <td><input type="checkbox"></td>
                 </tr>
                 
                 `
+
+                document.querySelectorAll('.googleMaps').forEach(function(link){
+
+                  link.addEventListener('click', function(event) {
+            
+                    event.preventDefault();
+                    const packageLatitude = datos_paquete.latitud;
+                    const packageLongitude = datos_paquete.longitud;
+            
+                    console.log(packageLatitude + " " + packageLongitude)
+
+
+                    const directionsService = new google.maps.DirectionsService();
+      
+                    // Configura la solicitud con la ubicación de origen y destino.
+                    const request = {
+                      origin: new google.maps.LatLng(-34.8681439523412, -56.166493557192815), // Cambia por tus coordenadas de origen
+                      destination: new google.maps.LatLng(packageLatitude, packageLongitude), // Coordenadas ingresadas por el usuario
+                      travelMode: google.maps.TravelMode.DRIVING, // Puedes cambiar el modo de viaje según tus necesidades
+                    };
+                  
+                    // Realiza la solicitud para calcular la ruta.
+                    directionsService.route(request, function (result, status) {
+                      if (status === google.maps.DirectionsStatus.OK) {
+                        // El resultado contiene la ruta más cercana.
+                        // Puedes mostrarlo en el mapa o procesarlo como desees.
+                        const map = new google.maps.Map(document.getElementById('map'), {
+                          zoom: 10,
+                          center: new google.maps.LatLng(-34.8681439523412, -56.166493557192815), // Centro del mapa
+                        });
+                        const directionsDisplay = new google.maps.DirectionsRenderer();
+                        directionsDisplay.setMap(map);
+                        directionsDisplay.setDirections(result);
+                      } else {
+                        console.error('Error al calcular la ruta: ' + status);
+                      }
+                    });
+                    
+            
+                  })
+            
+                })
 
             })
 
@@ -63,16 +110,4 @@ fetch(URL_PAQUETES_PARA_ENTREGAR)
         console.error('Ocurrió un error: ', error);
     }); 
 
-  /* fetch(URL_DETALLE_PAQUETE)
-  .then(response => {
-      if (!response.ok) {
-        throw new Error('La solicitud no pudo ser completada.');
-      }
-      return response.json(); // O response.text() si la respuesta no es JSON.
-    })
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error('Ocurrió un error: ', error);
-    }); */
+    });
